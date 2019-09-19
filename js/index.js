e875d43cb86268469c7e8036e9db975edfb26f1d
Tomek <tomekz@vp.pl>
2019-09-19 15:55:45 +0200

    Add more styling, add some JS functionality

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
 window.onload = () => {
 
+  const randomPokemonBtn = document.querySelector('#rndPkmnBtn');
+  const tenRndmPokemonBtn = document.querySelector('#tenRndmPkmBtn');
+  const randomPokemonNumber = () => Math.floor(Math.random() * (808 - 1)) + 1;
+
+  const getPokemon = (pokemonNumber, howManyOfPokemons) => {
+    for (let i = 0; i < howManyOfPokemons; i++) {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNumber()}/`)
+        .then(result => result.json())
+        .then(result => console.log(result.name, result.order));
+    }
+  };
+
+  randomPokemonBtn.addEventListener('click', () => {
+    getPokemon(randomPokemonNumber, 1);
+  });
+
+  tenRndmPokemonBtn.addEventListener('click', () => {
+    getPokemon(randomPokemonNumber, 10);
+  });
 };
