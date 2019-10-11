484be09a2964a2ed34d5c97bb593d959044ab5d2
Tomek <tomekz@vp.pl>
2019-10-11 16:05:14 +0200

    Format the stats chart

--- js/renderPokemonStats.js
+++ js/renderPokemonStats.js
@@ -3,17 +3,49 @@ const statsBox = document.querySelector('#stats');
 export const renderStats = new Chart(statsBox, {
   type: 'radar',
   data: {
-    labels: ['Running', 'Swimming', 'Eating', 'Cycling', 'joma'],
+    labels: ['HP', 'Defense', 'Sp. Defense', 'Speed', 'Sp. Attack', 'Attack'],
     datasets: [{
-      data: [9, 6, 4, 2, 6, 8],
-      backgroundColor: [
-        '#e6e6e6'
-      ],
-      borderColor: [
-        '#e6e6e6'
-      ],
-      borderWidth: 2
+      data: [45, 49, 65, 45, 65, 49],
+      backgroundColor: '#ffffffce',
+      borderColor: '#ffffffce',
+      borderWidth: 2,
+      pointBorderWidth: 3,
+      pointBackgroundColor: '#f05a4f',
+      pointBorderColor: '#ffffffce',
+      pointHoverRadius: 5
     }],
   },
-  options: Chart.defaults.radar
+  options: {
+    legend: {
+      display: false
+    },
+    scale: {
+      gridLines: {
+        color: '#ffffffce'
+      },
+      angleLines: {
+        color: '#ffffffce'
+      },
+      ticks: {
+        display: false,
+        beginAtZero: true,
+        min: 0,
+        max: 100,
+        stepSize: 20
+      },
+      pointLabels: {
+        fontSize: 12
+      }
+    },
+    tooltips: {
+      //Need to use this workaround, because of a bug. See: https://github.com/chartjs/Chart.js/issues/6188
+      enabled: true,
+      displayColors: false,
+      callbacks: {
+        label: function (tooltipItem, data) {
+          return data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index];
+        }
+      }
+    }
+  }
 });
